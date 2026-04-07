5f42e3f9c124e94240560f250f2d0d6fabbe0099
abdul sami <abdulsami170803@gmail.com>
2026-04-07 18:51:54 +0530

    Refactor color and size control logic in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const colors = ['black','violet','indigo','blue','green','yellow','orange','red'];
 
-let size =30;
+let size = 30;
 let size_left = document.querySelector('.size_left');
 let size_right = document.querySelector('.size_right');
 let size_box = document.querySelector('.size');
@@ -16,146 +16,146 @@ let rainbow = document.querySelector('.rainbow');
 
 let rain  = false;
 let col_idx = 0;
-let cir = 0;
+
 col_box.style.backgroundColor = colors[col_idx];
+col_text.innerText = colors[col_idx];
 
-
-
+// 🎨 Color controls
 col_left.addEventListener('click',()=>{
-    rain = 'false';
+    rain = false;
     eraser.style.backgroundColor = '#fff';
     eraser.style.color = 'black';
-    if(col_idx==0){
-        col_idx = 8;
-    }
-    if(col_idx>0){
-        col_idx--;
-        col_box.style.backgroundColor = colors[col_idx];
-        col_text.innerText = colors[col_idx];
-    }
+
+    if(col_idx === 0) col_idx = colors.length;
+    col_idx--;
+
+    col_box.style.backgroundColor = colors[col_idx];
+    col_text.innerText = colors[col_idx];
 });
 
 col_right.addEventListener('click',()=>{
-    rain = 'false';
+    rain = false;
     eraser.style.backgroundColor = '#fff';
     eraser.style.color = 'black';
-    if(col_idx==7){
-        col_idx = -1;
-    }
-    if(col_idx<7){
-        col_idx++;
-        col_box.style.backgroundColor = colors[col_idx];
-        col_text.innerText = colors[col_idx];
-    }
+
+    if(col_idx === colors.length - 1) col_idx = -1;
+    col_idx++;
+
+    col_box.style.backgroundColor = colors[col_idx];
+    col_text.innerText = colors[col_idx];
 });
 
-
+// 📏 Size controls
 size_box.innerText = size;
+
 size_left.addEventListener('click',()=>{
-    
-    if(size>1){
+    if(size > 1){
         size--;
         size_box.innerText = size;
     }
-})
+});
+
 size_right.addEventListener('click',()=>{
-    
-    if(size<60){
+    if(size < 60){
         size++;
         size_box.innerText = size;
     }
-})
+});
+
+// 🧱 Create grid
 set.addEventListener('click',()=>{
-    
-    rain = 'false';
+    rain = false;
     eraser.style.backgroundColor = '#fff';
     eraser.style.color = 'black';
+
     main_box.innerHTML = '';
-    let numb = size_box.innerText
-    
-    for(let i=0;i<numb*numb;i++){
+    let numb = size;
+
+    for(let i = 0; i < numb * numb; i++){
         let cell = document.createElement('div');
         cell.style.height = (600/numb)+'px';
-        cell.style.width = (600/numb)+ 'px';
-        
+        cell.style.width = (600/numb)+'px';
         cell.classList.add('child_div');
         main_box.appendChild(cell);
     }
-    let childern = document.querySelectorAll('.child_div');
+
     setTimeout(()=>{
-        for(const child of childern){
+        document.querySelectorAll('.child_div').forEach(child=>{
             child.style.border = 'none';
-        }
+        });
     },400);
-    changecol(childern);
-    
 });
 
-function changecol(childern){
-    for(const div of childern ){
-        
-        let temp=0;
-          
-        let isMouseDown = false;
-        div.addEventListener("mousedown", () => {
-            isMouseDown = true; 
-            if(rain=='true'){
-                if(temp==8){
-                    temp =0;
-                }
-                col_text.innerText = colors[temp];
-                temp++;
-            }else{
-                rainbow.style.backgroundColor = '#fff';
-                rainbow.style.color = 'black';
-            }
-            div.style.backgroundColor = col_text.innerText;
-        });
-        document.addEventListener("mousemove", (event) => {
-            if (isMouseDown ) { 
-                let box = event.target;
-                if(rain=='true'){
-                    if(temp==8){
-                        temp =0;
-                    }
-                    col_text.innerText = colors[temp];
-                    temp++;
-                }
-                if(box.classList.contains("child_div")){
-                    box.style.backgroundColor = col_text.innerText;
-                }
-                
-            }
-        });
-        document.addEventListener("mouseup", () => {
-            isMouseDown = false;
-        });
-    }
-}
+// 🖌️ Drawing logic (optimized)
+let isMouseDown = false;
+let temp = 0;
 
+document.addEventListener("mousedown", () => {
+    isMouseDown = true;
+});
+
+document.addEventListener("mouseup", () => {
+    isMouseDown = false;
+});
+
+main_box.addEventListener("mousemove", (event) => {
+    if (!isMouseDown) return;
+
+    let box = event.target;
+
+    if (box.classList.contains("child_div")) {
+        if (rain) {
+            if (temp === colors.length) temp = 0;
+            col_text.innerText = colors[temp];
+            temp++;
+        }
+        box.style.backgroundColor = col_text.innerText;
+    }
+});
+
+main_box.addEventListener("mousedown", (event) => {
+    let box = event.target;
+
+    if (box.classList.contains("child_div")) {
+        if (rain) {
+            if (temp === colors.length) temp = 0;
+            col_text.innerText = colors[temp];
+            temp++;
+        } else {
+            rainbow.style.backgroundColor = '#fff';
+            rainbow.style.color = 'black';
+        }
+        box.style.backgroundColor = col_text.innerText;
+    }
+});
+
+// 🧹 Clear
 clear.addEventListener('click',()=>{
-    rain = 'false';
+    rain = false;
     main_box.innerHTML = '';
     set.click();
 });
 
+// 🧽 Eraser
 eraser.addEventListener('click',()=>{
-    rain = 'false';
+    rain = false;
     rainbow.style.backgroundColor = '#fff';
     rainbow.style.color = 'black';
+
     eraser.style.backgroundColor = '#333333';
     eraser.style.color = '#fff';
+
     col_box.style.backgroundColor = 'white';
-    let childern = document.querySelectorAll('.child_div');
     col_text.innerText = 'white';
-    console.log("eraser");
-    changecol(childern);
 });
 
+// 🌈 Rainbow mode
 rainbow.addEventListener('click',()=> {
-    rain = 'true';
+    rain = true;
+
     rainbow.style.backgroundColor = '#333333';
     rainbow.style.color = '#fff';
+
     eraser.style.backgroundColor = '#fff';
     eraser.style.color = 'black';
-    });
+});
